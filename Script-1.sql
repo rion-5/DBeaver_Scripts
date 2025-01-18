9b977153b91c89e82c28a8866e022f9f97813c73
rion-5 <sklee@hanyang.ac.kr>
2025-01-18 21:21:49 +0900

    insert stock_data

--- Script-1.sql
+++ Script-1.sql
@@ -5,3 +5,25 @@ limit 10;
 
 
 delete from stock where trading_date = '2024-10-10';
+
+
+SELECT trading_date, close
+FROM stock
+WHERE symbol = 'TSLA'
+  AND trading_date >= CURRENT_DATE - INTERVAL '14 DAYS'
+ORDER BY trading_date;
+
+
+delete from stock_data;
+
+select trade_date, count(*) from stock_data
+group by  trade_date 
+order by trade_date desc;
+
+
+select * from stock where trading_date = '2025-01-17';
+
+select * from stock_data;
+
+insert into stock_data (ticker,trade_date,open_price,high_price,low_price, close_price, volume) 
+ select symbol,trading_date, open, high, low, close, volume from stock ;
